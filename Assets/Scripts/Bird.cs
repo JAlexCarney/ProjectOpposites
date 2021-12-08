cf88d809d2a95b6e14023f0740d035c78c942c58
JAlexCarney <JAlexCarney@gmail.com>
2021-12-07 19:49:16 -0600

    Changed Bird Movement

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -4,81 +4,114 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
+    public struct ActionPad {
+        public GameObject Marker;
+        public GameObject Pad;
+        public Vector3 MarkerPosition;
+
+        public ActionPad(GameObject m, GameObject p, Vector3 mp) {
+            Marker = m;
+            Pad = p;
+            MarkerPosition = mp;
+        }
+    }
+
     public GameObject Visual;
     public float moveSpeed = 5f;
     public Turtle turtle;
     public Hotbar hotbar;
     public GameObject[] prefabs;
+    public GameObject markerPrefab;
     public Transform prefabContainer;
+    public Queue<ActionPad> actionPads;
     private Rigidbody2D rb;
     private SpriteRenderer spriteRender;
     private Vector2 movement;
+    private bool playing = false;
 
     void Start()
     {
+        actionPads = new Queue<ActionPad>();
         rb = GetComponent<Rigidbody2D>();
         spriteRender = Visual.GetComponent<SpriteRenderer>();
     }
 
+    private void PlacePrefabDestroyMarker(ActionPad PadToPlace)
+    {
+        Destroy(PadToPlace.Marker);
+        GameObject Pad = Instantiate(PadToPlace.Pad, prefabContainer);
+        Pad.transform.position = PadToPlace.MarkerPosition;
+        movement = Vector2.zero;
+    }
+
     private void Update()
     {
-        // Input
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
-        if (movement.magnitude > 0f) 
-        {
-            turtle.StartWalking();
-            hotbar.Show();
-        }
-        if (movement.x > 0.1)
-        {
-            spriteRender.flipX = true;
-        }
-        else if (movement.x < -0.1)
-        {
-            spriteRender.flipX = false;
-        }
-
-        int objToPlace = -1;
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            objToPlace = 0;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2)) 
-        {
-            objToPlace = 1;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            objToPlace = 2;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha4))
-        {
-            objToPlace = 3;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha5))
-        {
-            objToPlace = 4;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha6))
-        {
-            objToPlace = 5;
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha7))
-        {
-            objToPlace = 6;
+        if (playing) {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                hotbar.Select(0);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                hotbar.Select(1);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                hotbar.Select(2);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                hotbar.Select(3);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha5))
+            {
+                hotbar.Select(4);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha6))
+            {
+                hotbar.Select(5);
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha7))
+            {
+                hotbar.Select(6);
+            }
         }
 
-        if (objToPlace != -1) 
+        if (Input.GetMouseButtonDown(0)) 
         {
-            GameObject go = Instantiate(prefabs[objToPlace], prefabContainer);
-            go.transform.position = new Vector3(transform.position.x, transform.position.y - 0.4f, 0f);
+            if (!playing)
+            {
+                playing = true;
+                turtle.StartWalking();
+                hotbar.Show();
+            }
+            else 
+            {
+                Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                GameObject marker = Instantiate(markerPrefab, prefabContainer);
+                marker.transform.position = new Vector3(worldPosition.x, worldPosition.y, 0f);
+                ActionPad actionPad = new ActionPad(marker, prefabs[hotbar.selected], marker.transform.position);
+                actionPads.Enqueue(actionPad);
+            }
         }
     }
 
     void FixedUpdate()
     {
-        // physics
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        if (actionPads.Count > 0) {
+            Vector3 goalPosition = actionPads.Peek().MarkerPosition;
+            Vector2 distanceToGoal = new Vector2(goalPosition.x - transform.position.x, goalPosition.y - transform.position.y);
+            if (distanceToGoal.magnitude > 0.5f)
+            {
+                movement = distanceToGoal.normalized;
+            }
+            else
+            {
+                PlacePrefabDestroyMarker(actionPads.Dequeue());
+            }
+
+            // physics
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        }
     }
 }
--- Assets/Scripts/Hotbar.cs
+++ Assets/Scripts/Hotbar.cs
@@ -6,10 +6,41 @@ public class Hotbar : MonoBehaviour
 {
     private bool hidden = true;
     private RectTransform rt;
+    private readonly int baseX = 63;
+    private readonly int offX = 130;
+    public int selected = 0;
+    private int numSlots = 7;
+    private RectTransform highlightrt;
 
     private void Start()
     {
         rt = GetComponent<RectTransform>();
+        highlightrt = transform.GetChild(0).GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (Input.mouseScrollDelta.y > 0.1)
+        {
+            Select((selected + 1) % numSlots);
+        }
+        else if (Input.mouseScrollDelta.y < -0.1) 
+        {
+            if (selected == 0)
+            {
+                Select(numSlots - 1);
+            }
+            else 
+            {
+                Select(selected - 1);
+            }
+        }
+    }
+
+    public void Select(int i)
+    {
+        highlightrt.anchoredPosition = new Vector2(baseX + i*offX, 0f);
+        selected = i;
     }
 
     public void Show() {
